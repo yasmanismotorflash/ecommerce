aa355f0333059281869f9a631154e16f05295f41
Leandro Hernández <leandrohernadezf@gmail.com>
2024-10-08 14:43:16 +0200

    user page

--- admin-front/src/app/[locale]/admin/users/page.tsx
+++ admin-front/src/app/[locale]/admin/users/page.tsx
@@ -1,18 +1,12 @@
-"use client";
-
-import { useTranslation } from '../../hooks/useTranslation';
-import LanguageSwitcher from '../../components/LanguageSwitcher';
-import Config  from '../../../../next.config.mjs';
-import {useLanguage} from "@/app/context/LanguageContext";
+import { useTranslations } from "next-intl";
 
 export default function UsersPage() {
-    const { locale } = useLanguage();
-    const translations = useTranslation(locale);
+    const t = useTranslations()
 
     return (
         <div>
-            <LanguageSwitcher locales={Config.i18n.locales} />
-            <h1>{translations.manageUsers}</h1>
+         
+            <h1>{t("title")}</h1>
         </div>
     );
 }
