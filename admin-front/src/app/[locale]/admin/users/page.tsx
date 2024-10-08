65ee8b5def9bea81eaee1b0983d411cc67f3dd2c
yasmanibasulto <yasmanis.basulto@motorflash.com>
2024-10-08 11:13:30 -0400

    fixed error traslation

--- admin-front/src/app/[locale]/admin/users/page.tsx
+++ admin-front/src/app/[locale]/admin/users/page.tsx
@@ -6,7 +6,7 @@ export default function UsersPage() {
     return (
         <div>
          
-            <h1>{t("title")}</h1>
+            <h1>{t("HomePage.title")}</h1>
         </div>
     );
 }
