d05ddbb34c8713dd50eddea803b14975664c56e7
Leandro Hernández <leandrohernadezf@gmail.com>
2024-10-10 22:36:15 +0200

    creando  ViewCard,ViewCardHeader,ViewCardBody,ViewCardTitle,ViewCardFooter

--- admin-front/next.config.mjs
+++ admin-front/next.config.mjs
@@ -4,7 +4,10 @@ const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ['mf-front'],  // Para permitir que Next.js procese el paquete
+    transpilePackages: ['mf-front'], 
+    images: {
+        domains: ['images.motorflash.com']
+    }
 };
  
 export default withNextIntl(nextConfig);
