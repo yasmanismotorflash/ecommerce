4af0089fb4e6300bd332843e472500ea6c54ddba
Eidy EV <eidy.estupinan@motorflash.com>
2024-10-16 18:18:38 -0400

    Imnplementada autenticación con JWT, agregados servicios complementarios, actualizado composer.json agregado datos iniciales para la cuenta admin

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -8,7 +8,8 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
--- backend/src/DataFixtures/AppFixtures.php
+++ backend/src/DataFixtures/AppFixtures.php
@@ -1,16 +1,26 @@
 <?php
-
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        //Agregar usuario admin.
+        $admin = new User();
+        $admin->setEmail('admin@apicore.local')
+              ->setPassword('$2y$13$3gPr7jQWdp8EdyUH6c2nhOwgs/Qb.suxfNKi21HQioI1.CoNb9q8m');
+
+        $manager->persist($admin);
+
+
+
+
 
         $manager->flush();
     }
