f2f559e20162cb2e60f191a65c101de7988913f8
Andro Ortiz Dieguez <andro.ortiz@motorflash.com>
2024-10-17 17:04:00 -0400

    cambios en el login

--- admin-front/src/app/[locale]/admin/login/page.tsx
+++ admin-front/src/app/[locale]/admin/login/page.tsx
@@ -5,8 +5,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import MfInput from '@/components/ui/mf/input';
 import MfButton from '@/components/ui/mf/button';
-import {PersonIcon} from '@radix-ui/react-icons'
-
+import {PersonIcon} from '@radix-ui/react-icons';
 export default function LoginPage() {
     const [error, setError] = useState<string | null>(null);
     const router = useRouter();
@@ -33,15 +32,15 @@ export default function LoginPage() {
 
     return (
         <div className='flex flex-col justify-center items-center h-screen'>
-            <div className='flex flex-col justify-center items-center border-solid border-2 border-gray-600 rounded-lg p-8'>
-                <h2 className='pb-5 flex flex-row'><PersonIcon className='size-5' />&nbsp;Login</h2>
-                
+            <div className='flex flex-col justify-center items-center rounded-2xl shadow-lg shadow-gray-400 p-8 bg-gray-100'>
+                    <div className='relative bg-gray-400 rounded-full p-4 shadow-md'><PersonIcon className='size-7' /></div>
+                    <h2 className='flex flex-row text-2xl'>Login</h2>
             <form onSubmit={handleSubmit} className='flex flex-col justify-center w-full'>
-                <MfInput type="email" name="email" placeholder="Email" required />
-                <MfInput type="password" name="password" placeholder="Password" required />
+                <MfInput type="email" name="email" placeholder="Email" required  className=' border-gray-500'/>
+                <MfInput type="password" name="password" placeholder="Password" required className=' border-gray-500' />
                 <MfButton 
                     type="submit"
-                    className='hover:bg-gray-700 hover:text-white'
+                    className='hover:bg-gray-700 hover:text-white w-full'
                 >Entrar</MfButton>
             </form>
             {error && <p>{error}</p>}
--- admin-front/src/app/api/auth/[...nextauth]/route.ts
+++ admin-front/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import { z } from 'zod';
 import { NextAuthOptions } from 'next-auth';
+import { error } from 'console';
+import { redirect } from "next/navigation";
 
 // Extiende el tipo de `session.user` para incluir la propiedad `id`
 declare module 'next-auth' {
@@ -13,6 +16,8 @@ declare module 'next-auth' {
         }
     }
 }
+
+
 
 const authOptions: NextAuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
@@ -27,17 +32,50 @@ const authOptions: NextAuthOptions = {
             id: 'credentials',
             name: 'Credentials',
             credentials: {
-                email: { label: 'Email', type: 'email' },
-                password: { label: 'Password', type: 'password' },
+                email: { label: 'email', type: 'email' },
+                password: { label: 'password', type: 'password' },
             },
-            async authorize(credentials) {
+            async authorize(credentials,req) {
                 if (!credentials) {
                     return null;
                 }
-                const user = { id: '1', name: 'Admin', email: credentials.email };
+
+                const parsedCredentials = z
+                .object({ 
+                    email: z.string().email(),
+                    password: z.string().min(3) 
+                })
+                .safeParse(credentials);
+                console.log(parsedCredentials)
+                if (parsedCredentials.success){
+                    const URI = process.env.API_URL
+                    console.log(`${URI}/auth`)
+                    const { email, password } = parsedCredentials.data;
+                    const query ={
+                        "email":email,
+                        "password":password,
+                    }
+                    
+                    if (!URI){
+                        throw error('Thereis no URI!!');
+                    }
+
+
+                    const res = await fetch(`${URI}/auth`, {
+                        method: 'POST',
+                        body: JSON.stringify(query),
+                        headers: { "Content-Type": "application/json" }
+                    })
+                    console.log('RES',res)
+                    if (res.status===200){
+                        redirect('/admin');
+                    }
+                }
+                //console.log(res)
+                /*const user = { id: '1', name: 'Admin', email: credentials.email };
                 if (credentials.email === 'admin@example.com' && credentials.password === 'password') {
                     return user;
-                }
+                }*/
                 return null;
             }
         })
@@ -51,6 +89,17 @@ const authOptions: NextAuthOptions = {
                 token.id = user.id;
             }
             return token;
+        },
+        async signIn({ email, credentials }) {
+            const isAllowedToSignIn = true
+            if (isAllowedToSignIn) {
+              return true
+            } else {
+              // Return false to display a default error message
+              return false
+              // Or you can return a URL to redirect to:
+              // return '/unauthorized'
+            }
         },
         async session({ session, token }) {
             if (session?.user && token?.id) {
