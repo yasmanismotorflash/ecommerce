4b44f5df9b7a79123f1c7d68b429e333f7872241
Andro Ortiz Dieguez <andro.ortiz@motorflash.com>
2024-10-31 12:01:26 -0400

    cambios en sidebar

---
+++ admin-front/src/app/[locale]/shadcnadmin/page.tsx
@@ -0,0 +1,11 @@
+export default function ShadcnAdmin() {
+    return (
+        <>
+            <div className="text-2xl p-4">
+                Admin con Shadcn
+
+            </div>
+
+        </>
+    );
+}
