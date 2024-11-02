ea036556df8167bf50a8e5c1f5c3e91d725d415a
Andro Ortiz Dieguez <andro.ortiz@motorflash.com>
2024-11-02 10:55:46 -0400

    cambios en la página principal

--- admin-front/src/app/[locale]/page.tsx
+++ admin-front/src/app/[locale]/page.tsx
@@ -1,8 +1,4 @@
 
-
-import React from 'react';
-import { MfSidebar } from '@/components/ui/mf/MfSidebar/MfSidebar';
-import { getLocales } from '@/lib';
 import { redirect } from 'next/navigation';
 import { getLocale } from 'next-intl/server';
 
@@ -11,15 +7,5 @@ export default async function HomePage() {
     console.log(locale);
     redirect(`/${locale}/admin`);
 
-    return (
-        <div className="flex min-h-screen">
-            {/* Sidebar */}
-            <MfSidebar />
-
-            {/* Contenido principal */}
-            <div className="flex-1 p-10 bg-white ">
-                <h1 className="text-3xl font-bold mb-8">{t('title')}</h1>
-            </div>
-        </div>
-    );
+    
 }
--- admin-front/src/lib/index.ts
+++
@@ -1,1 +0,0 @@
-export {getLocales} from './locale';
